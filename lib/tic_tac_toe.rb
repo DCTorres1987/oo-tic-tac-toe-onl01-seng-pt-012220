0def5d71b389dbf60e560a1ca7d18bf54334c35d
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-02-13 10:30:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,10 +1,14 @@
 require 'pry'
 
 class tictactoe
-  attr_accessor :board
+  attr_reader :board
   
- 
-  def initialize(board = nil)
+ WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5]  
+
+]
+  def initialize
     @board = board || Array.new(9, " ")
   
   end
